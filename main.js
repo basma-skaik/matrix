22973dcfdbc0cb769edcf15556bba10e5ff715ed
basma-skaik <basmahskaik@gmail.com>
2022-06-14 00:25:47 +0300

    upload all files to github Relate #1

---
+++ main.js
@@ -0,0 +1,74 @@
+function over(){
+    let s = document.getElementsByClassName("con")[0]
+    let s2 = document.getElementsByClassName("inner")[0]
+    s.style.color ='rgba(0, 195, 255, 0.884)'
+    s2.style.color = 'white' 
+}
+function over2(){
+    let s3 = document.getElementsByClassName("con")[1]
+    let s4 = document.getElementsByClassName("inner")[1]
+    s3.style.color ='rgba(0, 195, 255, 0.884)'
+    s4.style.color = 'white'
+}
+function over3(){
+    let s5 = document.getElementsByClassName("con")[2]
+    let s6 = document.getElementsByClassName("inner")[2]
+    s5.style.color ='rgba(0, 195, 255, 0.884)'
+    s6.style.color = 'white'
+}
+function out(){
+    let s = document.getElementsByClassName("con")[0]
+    let s2 = document.getElementsByClassName("inner")[0]
+    s.style.color = 'rgb(207, 203, 203)';
+    s2.style.color = 'rgb(207, 203, 203)';   
+}
+function out2(){
+    let s3 = document.getElementsByClassName("con")[1]
+    let s4 = document.getElementsByClassName("inner")[1]
+    s3.style.color = 'rgb(207, 203, 203)';
+    s4.style.color = 'rgb(207, 203, 203)';
+}
+function out3(){
+    let s5 = document.getElementsByClassName("con")[2]
+    let s6 = document.getElementsByClassName("inner")[2]
+    s5.style.color = 'rgb(207, 203, 203)';
+    s6.style.color = 'rgb(207, 203, 203)';
+}
+
+
+function overI1(){
+    let x = document.getElementById("con1")
+    let x2 = document.getElementById("inner1")
+    x.style.backgroundColor = '#1da1f2'
+    x2.style.color = 'white'
+}
+function outI1(){
+    let x3 = document.getElementById("con1")
+    let x4 = document.getElementById("inner1")
+    x3.style.backgroundColor = 'white'
+    x4.style.color = '#1da1f2'
+}
+function overI2(){
+    let x5 = document.getElementById("con2")
+    let x6 = document.getElementById("inner2")
+    x5.style.backgroundColor = '#1da1f2'
+    x6.style.color = 'white'
+}
+function outI2(){
+    let x7 = document.getElementById("con2")
+    let x8 = document.getElementById("inner2")
+    x7.style.backgroundColor = 'white'
+    x8.style.color = '#1da1f2'
+}
+function overI3(){
+    let x9 = document.getElementById("con3")
+    let x10 = document.getElementById("inner3")
+    x9.style.backgroundColor = '#1da1f2'
+    x10.style.color = 'white'
+}
+function outI3(){
+    let x11 = document.getElementById("con3")
+    let x12 = document.getElementById("inner3")
+    x11.style.backgroundColor = 'white'
+    x12.style.color = '#1da1f2'
+}
